5ad61b682cbe52adc639ca25d3e42ead950c6913
David Mendoza <mendoza.line@aim.com>
2017-08-25 17:48:11 -0700

    Refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,96 +3,151 @@ import { Map, TileLayer, CircleMarker, Popup } from 'react-leaflet'
 import * as d3 from 'd3'
 
 class App extends Component {
+
   constructor() {
     super()
 
     this.state = {
       lat: 45.4889,
       lng: -122.806377,
-      zoom: 12,
-
       points: null,
+      zoom: 10,
+      height: 450,
+      width: 900,
+      cuisine: 'All',
     } //end of state
 
   } //end constructor
 
   componentDidMount() {
+    console.log('cDM')
     let cmp = this
     const url = `https://cdn.glitch.com/1ffed53a-97b5-4e8d-9ce0-9a9b638459eb%2Fcombined.geojson`
 
     d3.queue()
       .defer(d3.json, url)
-      .await((error, dataPts) => {
-        //console.log('points-load:', dataPts)
+      .await((error, geoPoints) => {
+        //console.log('d3-points:', geoPoints)
 
         cmp.setState({
-          points: dataPts,
-        }) //end setState
+          points: geoPoints
+        })
 
       }) //end await
 
-  } //end CDM
-
+  }
 
   render() {
+    console.log('render')
+
     if(this.state.points == null) {
+      console.log('points: null')
       return null
     }
 
-    let pts = this.state.points
-    //console.log('points-render', this.state.points)
+    const pts = this.state.points
+    //console.log('points:', this.state.points)
+    //console.log('state.cuisine:', this.state.cuisine)
+    console.log('w', this.state.width, 'h', this.state.height)
+
+    /* filter data */
+    const filterData = (cuisineSelected) => {
+      if(cuisineSelected === 'All') {
+        return this.state.points.features
+      }
+
+      const data = pts.features.filter(type => {
+        return type.properties.cuisines[0] === cuisineSelected
+      })
+      return data
+    }
+
+    //console.log(this.state.cuisine, 'data:', filterData(this.state.cuisine))
+
+    const cuisineData = filterData(this.state.cuisine)
+    console.log('cuisine data:', cuisineData)
+
+
+    /* d3 functions */
+    let getColor = d3.scaleThreshold()
+      .domain([70, 80, 90, 100])
+      .range(['dimGray', 'crimson', 'darkOrange', 'gold', 'limeGreen'])
+
+    let getRadius = d3.scaleThreshold()
+      .domain([70, 80, 90, 100])
+      .range([1, 5, 10, 15, 20])
+
+
+    /* map circle markers */
+    const mapDots = cuisineData.map((feat, index) => {
+      //console.log(pt.geometry.coordinates) // -> [lng, lat]
+      let lat = feat.geometry.coordinates[1]
+      let lon = feat.geometry.coordinates[0]
+      let coord = [lat,lon]
+
+      const score = feat.properties.inspections[0].inspectScore
+      let inspectScore
+      if(score !== "Not scored") {
+        inspectScore = score
+      } else {
+        inspectScore = 69 //lower than min score possible
+      }
+
+      const fillColor = getColor(inspectScore) //fill
+      const strokeColor = getColor(inspectScore) //stroke
+      const radius = getRadius(inspectScore)
+      const cuisine = feat.properties.cuisines[0]
+
+      const restName = feat.properties.name
+      const yelpLink = feat.properties.yelpLink
+      const yelpRating = feat.properties.yelpRating
+      const formatScore = inspectScore !== 69 ? inspectScore + ' / 100': 'Not scored'
+
+      return (
+        <CircleMarker
+          key={index}
+          center={coord}
+          fillColor={fillColor}
+          color={strokeColor}
+          radius={radius}
+          >
+          <Popup>
+            <div className='popups'>
+              Name: <b><a href={yelpLink}>{restName}</a></b>
+              <br/>
+              Type: <b>{cuisine}</b>
+              <br/>
+              Health score: <b>{formatScore}</b>
+              <br/>
+              Yelp score: <b>{yelpRating} / 5</b>
+            </div>
+          </Popup>
+        </CircleMarker>
+      )
+    })
 
     const position = [this.state.lat, this.state.lng]
 
     return (
-      <Map center={position} zoom={this.state.zoom}>
-        <TileLayer
-          url="http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}"
-          ext="png"
-          subdomains="abcd"
-          minZoom={7}
-          maxZoom={17}
-          attribution="Map tiles by <a href='http://stamen.com'>Stamen Design</a>, <a href='http://creativecommons.org/licenses/by/3.0'>CC BY 3.0</a> &mdash; Map data &copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
-        />
-        {
-          pts.features.map((pt, index) => {
-            //console.log('feat', pt)
-            //console.log(pt.geometry.coordinates) // -> [lng, lat]
+      <div>
+        <Map center={position} zoom={this.state.zoom}>
+          <TileLayer
+            url="http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}"
+            ext="png"
+            subdomains="abcd"
+            minZoom={9}
+            maxZoom={20}
+            attribution="Map tiles by <a href='http://stamen.com'>Stamen Design</a>, <a href='http://creativecommons.org/licenses/by/3.0'>CC BY 3.0</a> &mdash; Map data &copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+            />
 
-            let lat = pt.geometry.coordinates[1]
-            let lon = pt.geometry.coordinates[0]
-            let coord = [lat,lon]
-            return (
-              <CircleMarker
-                key={index}
-                center={coord}
-                radius={10}
-                color={'magenta'}
-                fillColor={'yellow'}
-                >
-                <Popup>
-                  <div>
-                    <h2>Name: {pt.properties.name}</h2>
-                    <br/>
-                    <h4>
-                      Score: {pt.properties.inspections[0].inspectScore}
-                    </h4>
-                  </div>
-                </Popup>
+          { mapDots }
+        </Map>
+      </div>
 
-              </CircleMarker>
-
-              )
-          }) //end pts.map
-
-        }
-
-
-      </Map>
     ) // end return
 
   } // end render
 
 } // end component
 
-export default App;
+export default App
